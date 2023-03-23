6e8a968c0475835bcbe8b6c881c332505a8b9a0a
Lucas Bonanni <lbonanni@eventbrite.com>
2023-03-23 18:40:33 -0300

    Change the user controller

--- src/main/java/com/example/users/UsersApplication.java
+++ src/main/java/com/example/users/UsersApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UsersApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
 	}
--- src/main/java/com/example/users/controllers/UserController.java
+++ src/main/java/com/example/users/controllers/UserController.java
@@ -2,15 +2,43 @@ package com.example.users.controllers;
 
 import com.example.users.models.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    public ResponseEntity<User> Post(@RequestBody User user) {
+    public ResponseEntity<User> Create(@RequestBody User user) {
         return ResponseEntity.ok(new User());
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<User>> Get(){
+        return ResponseEntity.ok(new ArrayList<>() {
+            {
+               add(new User());
+               add(new User());
+               add(new User());
+            }
+        });
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> GetById(@PathVariable UUID id){
+        return ResponseEntity.ok(new User());
+    }
+
+    @PutMapping(path = "/{id}", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<User> Update(@PathVariable UUID id, @RequestBody User user){
+        return ResponseEntity.ok(new User());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> Delete(@PathVariable UUID id){
+        return ResponseEntity.ok(null);
     }
 }
